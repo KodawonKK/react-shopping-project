9de6ed5a4aeb540f9f3051b8cf9ce54aa7e5a146
KodawonKK <rhekdnjs0169@gmail.com>
2025-03-31 14:57:23 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import ProductPage from "./page/ProductPage";
 import ProductDetailPage from "./page/ProductDetailPage";
+import LoginPage from "./page/LoginPage";
+import UserPage from "./page/UserPage";
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false);
+  const PrivateRoute = () => {
+    return authenticate ? <UserPage /> : <Navigate to="/login" />;
+  };
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/products" element={<ProductPage />} />
-      <Route path="/products/:id" element={<ProductDetailPage />} />
+      <Route path="/products/:id/:num" element={<ProductDetailPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/user" element={<PrivateRoute />} />
     </Routes>
   );
 }
