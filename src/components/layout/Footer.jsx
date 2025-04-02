461ea8817de97c1ae885ff20fa5ec3fcee2aeb45
KodawonKK <rhekdnjs0169@gmail.com>
2025-04-02 15:54:04 +0900

    fix : 푸터 글자및 스타일 수정정

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,25 +7,59 @@ const FooterWrap = styled.div`
   padding: 30px 40px;
   display: flex;
 `;
-const FooterRight = styled.div`
-  padding: 0 10px;
+const Tel = styled.h1`
+  font-size: 30px;
+  font-weight: 400;
+`;
+const Info = styled.p`
+  font-size: 13px;
+  color: #7c7b7b;
 `;
 const FooterLeft = styled.div`
   padding: 0 10px;
 `;
+const PolicyWrap = styled.div`
+  display: flex;
+`;
+const PolicyItem = styled.span`
+  padding-right: 20px;
+  font-size: 14px;
+  color: #7c7b7b;
+  &.point {
+    color: #000;
+  }
+`;
+const FooterRight = styled.div`
+  padding: 0 10px;
+`;
+const EtcWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: #7c7b7b;
+`;
+const EtcItem = styled.span``;
 
 const Footer = () => {
   return (
     <FooterWrap>
       <FooterLeft>
         <img src={Logo} alt={"로고"} />
-        <h3>1670-9910</h3>
-        <p>평일 AM 10:00 - PM 05:00 / 점심 PM 12:00 - PM 01:00 / 토요일, 일요일, 공휴일 휴무</p>
+        <Tel>1670-9910</Tel>
+        <Info>평일 AM 10:00 - PM 05:00 / 점심 PM 12:00 - PM 01:00 / 토요일, 일요일, 공휴일 휴무</Info>
+        <Info>(주)이랜드월드패션사업부　대표이사 : 조동주,최종양　사업자등록번호 : 113-85-19030 </Info>
+        <PolicyWrap>
+          <PolicyItem>이용약관</PolicyItem>
+          <PolicyItem className="point">개인정보처리방침</PolicyItem>
+          <PolicyItem>이용안내</PolicyItem>
+        </PolicyWrap>
       </FooterLeft>
       <FooterRight>
         <h3>ABOUT US</h3>
-        <span>브랜드소개</span>
-        <span>매장정보</span>
+        <EtcWrap>
+          <EtcItem>브랜드소개</EtcItem>
+          <EtcItem>매장정보</EtcItem>
+        </EtcWrap>
       </FooterRight>
     </FooterWrap>
   );
